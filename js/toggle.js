4004bf4c916cb1469f14d2c8615004feee6a83e4
David Cinco <cincoISC@gmail.com>
2021-02-24 17:35:52 -0700

    commit ONe

---
+++ js/toggle.js
@@ -0,0 +1,17 @@
+function checkTime(){
+    let toggle = document.getElementById("checkbox");
+    let basicPrice = document.getElementById("basicPrice");
+    let professionalPrice = document.getElementById("professionalPrice");
+    let masterPrice = document.getElementById("masterPrice");
+
+    if(toggle.checked == false){
+        basicPrice.innerHTML = "&dollar;199.99";
+        professionalPrice.innerHTML = "&dollar;249.99";
+        masterPrice.innerHTML = "&dollar;399.99";
+    }
+    else{
+        basicPrice.innerHTML = "&dollar;19.99";
+        professionalPrice.innerHTML = "&dollar;24.99";
+        masterPrice.innerHTML = "&dollar;39.99";
+    }
+}
